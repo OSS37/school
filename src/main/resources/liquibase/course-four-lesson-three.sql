35b6cf8f412225493574e66ea9aa98acf8387465
OSS37 <suhanova375@gmail.com>
2023-07-19 15:48:59 +0300

    ДЗ "Миграции и индексы" исправлено

--- src/main/resources/liquibase/course-four-lesson-three.sql
+++ src/main/resources/liquibase/course-four-lesson-three.sql
@@ -1,13 +1,13 @@
---liquibase formated sql
+--liquibase formatted sql
 
 --changeset sukhanova:1
 --precondition-sql-check expectedResult:0 select count (*) from pg_catalog.pg_tables t inner join pg_indexes i on i.tablename = t.tablename where t.tablename = 'students' and i.indexname = 'IDX_students_name';
 --rollback DROP INDEX IDX_students_name
 
-CREATE INDEX IDX_students_name ON students(name);
+CREATE INDEX IDX_students_name ON students (name);
 
 --changeset sukhanova:2
 --precondition-sql-check expectedResult:0 select count(*) from pg_catalog.pg_tables t inner join pg_indexes i on i.tablename = t.tablename where t.tablename = 'faculties'and i.indexname = 'IDX_faculties_name_color';
---rollback DROP INDEX IDX_students_name
+--rollback DROP INDEX IDX_faculties_name_color
 
-CREATE INDEX IDX_faculties_name_color ON faculties (name, color);
+CREATE INDEX IDX_faculties_name_color ON faculties(name, color);
